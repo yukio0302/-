e5f4644012fb4987abf406bc733a323295ff50d6
yukio0302 <pika.pika.yukiyan@gmail.com>
2024-12-11 17:07:55 +0900

    Update cyasu.py

--- cyasu.py
+++ cyasu.py
@@ -4317,7 +4317,6 @@ st.markdown(
 ["西の関"]
 ]  # 1つの店舗で複数銘柄を取り扱い可能に
 })
-
 # OpenCage APIの設定
 api_key = "d63325663fe34549885cd31798e50eb2"
 geocoder = OpenCageGeocode(api_key)
@@ -4328,7 +4327,7 @@ st.write("最寄り駅を入力して、10km圏内の加盟店を検索します
 station_name = st.text_input("最寄り駅名を入力してください（「駅」は省略可能です）:")
 
 # デフォルトの地図
-m = folium.Map(location=[35.681236, 139.767125], zoom_start=5, tiles="Stamen Toner")  # Stamen Tonerを使用
+m = folium.Map(location=[35.681236, 139.767125], zoom_start=5, tiles="CartoDB Positron")  # CartoDB Positronを使用
 
 if station_name:
     search_query = station_name if "駅" in station_name else station_name + "駅"
@@ -4352,7 +4351,7 @@ if station_name:
         search_lat = selected_result['geometry']['lat']
         search_lon = selected_result['geometry']['lng']
 
-        m = folium.Map(location=[search_lat, search_lon], zoom_start=15, tiles="Stamen Toner")
+        m = folium.Map(location=[search_lat, search_lon], zoom_start=15, tiles="CartoDB Positron")
         folium.Marker([search_lat, search_lon], popup=f"{station_name}駅", icon=folium.Icon(color="red", icon="info-sign")).add_to(m)
 
         加盟店_data["distance"] = 加盟店_data.apply(
