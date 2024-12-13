e70c58896c29d8798af5482a6af840ee5cd667e7
yukio0302 <pika.pika.yukiyan@gmail.com>
2024-12-13 21:52:50 +0900

    Update cyasu.py

--- cyasu.py
+++ cyasu.py
@@ -10,7 +10,7 @@ import streamlit as st
 # カスタムCSSを適用
 st.image("kensakup_top.png", use_column_width=True)
 st.image("kensakup_topmain.png", use_column_width=True)
-st.image("to_kousikip.png", use_column_width=True)
+st.image("to-kousikip.png", use_column_width=True)
 st.markdown("""
     <style>
         /* 背景色を白、テキスト色を黒に設定 */
