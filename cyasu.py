b99d5e37e0007ce9be160d19d70da0d78f221eb7
yukio0302 <pika.pika.yukiyan@gmail.com>
2024-12-14 21:45:53 +0900

    Update cyasu.py

--- cyasu.py
+++ cyasu.py
@@ -10,9 +10,7 @@ import streamlit as st
 # カスタムCSSを適用
 st.image("kensakup_top.png", use_column_width=True)
 st.image("kensakup_topmain.png", use_column_width=True)
-st.markdown(
-    f'<a href="{https://www.meimonshu.jp/modules/xfsection/article.php?articleid=377}" target="_blank"><img src="{ st.image("to-kousikip.png", use_column_width=True)}" style="width:100%;"></a>',
-    unsafe_allow_html=True)
+ st.image("to-kousikip.png", use_column_width=True)
 st.image("kensakup_to-map.png", use_column_width=True)
 st.markdown("""
     <style>
